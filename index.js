1db28412e6dbdd56bbf16b2a22533adec1c1ad90
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-03-04 17:10:39 -0500

    Better injection point

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
-const { getModule, getModuleByDisplayName } = require('powercord/webpack');
-const { waitFor } = require('powercord/util');
+const { getModule, React } = require('powercord/webpack');
 
 module.exports = class NSFWtags extends Plugin {
-  async startPlugin() {
-    this.loadStylesheet('style.scss');
-    this.channels = await getModule(['getChannels']);
-    this.currentGuild = await getModule(['getLastSelectedGuildId']);
+	async startPlugin() {
+		this.loadStylesheet('style.scss');
+		const ChannelItem = getModule(m => m.default && m.default.displayName == 'ChannelItem', false);
 
-    let oldGuildId;
-    const _this = this
-    const AnalyticsContext = await getModuleByDisplayName('AnalyticsContext')
-    inject("NSFWtags", AnalyticsContext.prototype, 'renderProvider', function (_, res) {
-      if (this.props.page !== 'Guild Channel') {
-        oldGuildId = undefined
-        return res;
-      }
-      if (_this.currentGuild.getGuildId() !== oldGuildId) {
-        oldGuildId = _this.currentGuild.getGuildId();
-        _this.injectTag();
-      }
-      return res;
-    })
-  }
-  pluginWillUnload() {
-    uninject("NSFWtags");
-    document.querySelectorAll('.nsfw-badge').forEach(e => e.remove())
-  }
-  async injectTag() {
-    await waitFor('[aria-label="Channels"]') //waiting for the channel list to load
-    const guildChannel = this.channels.getChannels(this.currentGuild.getGuildId()).SELECTABLE;
-    const e = guildChannel.filter(c => c.channel.nsfw);
-    if (e.length === 0) return;
-    e.forEach(c => {
-      const element = document.querySelector(`[aria-label="${c.channel.name} (text channel)"]`) || document.querySelector(`[aria-label="unread, ${c.channel.name} (text channel)"]`);
-      if (element.parentElement.children[1].innerHTML.includes(`<div class="nsfw-badge"><div class="nsfw-text">NSFW</div></div>`)) return;
-      element.parentElement.children[1].innerHTML += `<div class="nsfw-badge"><div class="nsfw-text">NSFW</div></div>`; //Adds the NSFW tag
-    });
-  }
-}
+		inject('NSFWtags', ChannelItem, 'default', (_, props) => {
+			const children = props.props.children.props.children[1].props.children[1].props.children;
+			const channel = children[1].props.channel;
+			if (!channel.nsfw) return props;
+			children.unshift(
+        React.createElement('div', { className: 'nsfw-badge' }, 
+          React.createElement('div', { className: 'nsfw-text' }, 'NSFW')));
+			return props;
+		});
+		ChannelItem.default.displayName = 'ChannelItem';
+	}
+	pluginWillUnload() {
+		uninject('NSFWtags');
+		document.querySelectorAll('.nsfw-badge').forEach(e => e.remove());
+	}
+};
