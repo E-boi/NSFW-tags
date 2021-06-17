fec1a0ab01a54605b2ff68acd26bd06cfe8eab2c
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-06-17 19:21:25 -0400

    use css instead of scss

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ const { getModule, React } = require('powercord/webpack');
 
 module.exports = class NSFWtags extends Plugin {
 	async startPlugin() {
-		this.loadStylesheet('style.scss');
+		this.loadStylesheet('style.css');
 		const ChannelItem = getModule(m => m.default && m.default.displayName == 'ChannelItem', false);
 
 		inject('NSFWtags', ChannelItem, 'default', (_, props) => {
 			const children = props.props.children.props.children[1].props.children[1].props.children;
 			const channel = children[1].props.channel;
 			if (!channel.nsfw) return props;
-			children.unshift(
-        React.createElement('div', { className: 'nsfw-badge' }, 
-          React.createElement('div', { className: 'nsfw-text' }, 'NSFW')));
+			children.unshift(React.createElement('div', { className: 'nsfw-badge' }, React.createElement('div', { className: 'nsfw-text' }, 'NSFW')));
 			return props;
 		});
 		ChannelItem.default.displayName = 'ChannelItem';
